bbbc01806cb9cef999e558851a50a1ec2d32fb28
xhayper <hayper1919@gmail.com>
2022-01-19 10:36:55 +0700

    Update init.lua
    Fix the wrong type

--- src/init.lua
+++ src/init.lua
@@ -22,7 +22,7 @@ local function SanitizeControl(s: string): string
 	return string.gsub(s, "[\0\1\2\3\4\5\6\7\8\11\12\13\14\15\16\17\18\19\20\21\22\23\24\25\26\27\28\29\30\31]+", "")
 end
 
-local function highlight(textObject: Instance, src: string?)
+local function highlight(textObject: TextBox, src: string?)
 	src = SanitizeTabs(SanitizeControl(src or textObject.Text))
 
 	textObject.RichText = false
@@ -176,7 +176,7 @@ return setmetatable({
 	UpdateColors = updateColors,
 	Highlight = highlight
 }, {
-	__call = function(_, textObject: Instance, src: string?)
+	__call = function(_, textObject: TextBox, src: string?)
 		return highlight(textObject, src)
 	end
 })
